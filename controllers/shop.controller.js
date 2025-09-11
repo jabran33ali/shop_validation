02c9b66e3bb6424c1b7671e32f5ce5b862dd5ae8
Jibran Ali <jabran33ali@gmail.com>
2025-09-11 14:39:11 +0500

    debugging

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -469,9 +469,9 @@ export const getVisitCounts = async (req, res) => {
           assignedTo: id,
           visit: false,
         });
-
+        console.log(visitedCount, notVisitedCount);
         total = visitedCount + notVisitedCount;
-
+        console.log(total);
         return res.status(200).json({
           message: "Visit counts for auditor fetched successfully",
           visited: visitedCount,
